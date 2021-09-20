548b581576d0da2d0c5bd686c70247f0f6611e52
Melody Jin <jinxinyi@Melody-J.local>
2021-09-20 15:33:09 +0800

    correct the definition for T in two-sided test; Include F1 score

--- computeSummary3.R
+++ computeSummary3.R
@@ -1,11 +1,13 @@
 computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.describe, 
-                           seed.number, cutoff=0.05, perm.size=1000, auc.values){
+                           seed.number, cutoff=0.05, perm.size=1000, auc.values,
+                           logFC.df=NULL, statistic="os.t"){
   #setwd("/Users/MelodyJin/Desktop/MAST90108/MDSresearchproject/simulation_plots")
-  summ<- as.data.frame(matrix(NA, nrow =ncol(obs.p)*2 , ncol =14))
+  summ<- as.data.frame(matrix(NA, nrow =ncol(obs.p)*2 , ncol =17))
   colnames(summ)<- c("group","cell.type","num.cells", "test.name", "num.true.upreg","num.upreg",
-                         "TP", "precision", "sensitivity","overlap","cutoff","perm.size","seed.number","AUC")
+                         "TP", "precision", "sensitivity","F1","overlap","TP.overlap","cutoff","perm.size","seed.number","AUC","statistic")
   summ[, "group"]<- main.describe
   summ[, "cutoff"]<- cutoff
+  summ[, "statistic"]<- statistic
   summ[, "perm.size"]<- perm.size
   summ[, "seed.number"]<- seed.number
   summ[, "cell.type"]<- rep(colnames(obs.p), 2)
@@ -14,15 +16,36 @@ computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.descri
   summ[1:ncol(obs.p), "test.name"]<- rep("limma", ncol(obs.p))
   #summ[ncol(obs.p)+1: ncol(obs.p)*2, "test.name"]<- rep("permutation", ncol(obs.p))
   
+  if (statistic == "lfc.p"){
+    summ[summ$test.name == "limma", "statistic"]<- "os.t"
+  }
+  
   for (i in 1: ncol(obs.p)){
-    # positive genes 
-    limma_upreg_genes =  row.names(obs.p[which(obs.p[,i] < cutoff),])
-    perm_upreg_genes =  row.names(perm.p[which(perm.p[,i] < cutoff),])
-    
-    # negative genes 
-    limma_nonsig_genes =  row.names(obs.p[which(obs.p[,i]>=(1-cutoff)),])
-    perm_nonsig_genes =  row.names(perm.p[which(perm.p[,i]>=(1-cutoff)),])
-    
+    if (substr(statistic,1,2) == "ts"){
+      if (is.null(logFC.df)){
+        stop("No available logFC data")
+      }
+      # positive genes 
+      limma_upreg_genes =  intersect(row.names(obs.p[which(obs.p[,i] < cutoff),]),
+                                     row.names(logFC.df[which(logFC.df[,i] > 0),]))
+      perm_upreg_genes =  intersect(row.names(perm.p[which(perm.p[,i] < cutoff),]), 
+                                    row.names(logFC.df[which(logFC.df[,i] > 0),]))
+      
+      # negative genes 
+      limma_nonsig_genes =  intersect(row.names(obs.p[which(obs.p[,i] >=cutoff),]),
+                                      row.names(logFC.df[which(logFC.df[,i] <= 0),]))
+      perm_nonsig_genes =  intersect(row.names(perm.p[which(perm.p[,i] >= cutoff),]), 
+                                     row.names(logFC.df[which(logFC.df[,i] <= 0),]))
+      
+    }else{
+      # positive genes 
+      limma_upreg_genes =  row.names(obs.p[which(obs.p[,i] < cutoff),])
+      perm_upreg_genes =  row.names(perm.p[which(perm.p[,i] < cutoff),])
+      
+      # negative genes 
+      limma_nonsig_genes =  row.names(obs.p[which(obs.p[,i]>=(1-cutoff)),])
+      perm_nonsig_genes =  row.names(perm.p[which(perm.p[,i]>=(1-cutoff)),])
+    }
     # metrics 
     tp.l<- length(intersect(limma_upreg_genes, true.upreg[[i]]))
     fp.l<- length(limma_upreg_genes)-length(intersect(true.upreg[[i]], limma_upreg_genes))
@@ -33,6 +56,12 @@ computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.descri
     fp.p<- length(perm_upreg_genes)-length(intersect(true.upreg[[i]], perm_upreg_genes))
     tn.p<- length(intersect(setdiff(row.names(obs.p),true.upreg[[i]]),perm_nonsig_genes))
     fn.p<- length(setdiff(true.upreg[[i]], perm_upreg_genes))
+    
+    precision.p<- round(tp.p/(tp.p+fp.p),5)
+    precision.l<- round(tp.l/(tp.l+fp.l),5)
+    # recall=TruePositives / (TruePositives + FalseNegatives)
+    sensitivity.p<-  round(tp.p/(tp.p+fn.p),5)
+    sensitivity.l<-  round(tp.l/(tp.l+fn.l),5)
     
     summ[i, "num.cells"]<- cell_num[i]
     summ[i+ncol(obs.p), "num.cells"]<- cell_num[i]
@@ -54,9 +83,17 @@ computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.descri
     
     #summ[i, "FPR"]<- round(fp.l/(fp.l+tn.l), 5)
     #summ[i+ncol(obs.p), "FPR"]<- round(fp.p/(fp.p+tn.p), 5)
+    summ[i, "F1"]<- round((2 * precision.l * sensitivity.l) / (precision.l + sensitivity.l), 5)
+    summ[i+ncol(obs.p), "F1"]<- round((2 * precision.p * sensitivity.p) / (precision.p + sensitivity.p), 5)
+    
     
     summ[i, "overlap"]<- length(intersect(limma_upreg_genes, perm_upreg_genes))
     summ[i+ncol(obs.p), "overlap"]<- length(intersect(limma_upreg_genes, perm_upreg_genes))
+    
+    summ[i, "TP.overlap"]<- length(intersect(intersect(limma_upreg_genes, true.upreg[[i]]), 
+                                             intersect(perm_upreg_genes, true.upreg[[i]])))
+    summ[i+ncol(obs.p), "TP.overlap"]<- length(intersect(intersect(limma_upreg_genes, true.upreg[[i]]), 
+                                                         intersect(perm_upreg_genes, true.upreg[[i]])))
     
     summ[i, "AUC"]<- auc.values[i]
     summ[i+ncol(obs.p), "AUC"]<- auc.values[i+ncol(obs.p)]
@@ -122,9 +159,12 @@ computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.descri
          theme(legend.position= "none")+
          xlab(""))
   
+
+  
   plot(ggplot(df.pv[df.pv$true.de==1, ],
               aes(y=pvalue, col=test.name,x = test.name))+
-         geom_violin(draw_quantiles = c(0.5), scale="count")+facet_grid(~group)+geom_hline(yintercept =0.05, col="black"))
+         geom_violin(draw_quantiles = c(0.5), scale="count")+facet_grid(~group)+
+         geom_hline(yintercept =0.05, col="black"))
   
   
   for (i in 1:ncol(obs.p)){
@@ -132,21 +172,21 @@ computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.descri
     limma_upreg_genes =  row.names(obs.p[which(obs.p[,i]<0.05),])
     perm_upreg_genes =  row.names(perm.p[which(perm.p[,i]<0.05),])
     
-    #plot(perm.p[, i],obs.p[, i],     
-    #     lwd=0.1, pch=".", 
-    #     xlab =(paste(adj, "pvalue by permutation")), 
-    #     ylab=(paste(adj,"pvalue by limma")), 
-    #     main=(paste(colnames(obs.p)[i],"cells")))
-    #abline(a = 0,b=1, col="blue", lwd = 1.5)
+    df<- as.data.frame(cbind(obs = obs.p[,i], perm = perm.p[,i]))
+    row.names(df)<- row.names(obs.p)
+    df$true.DE<- 0
+    df[true.upreg[[i]], "true.DE"]<- 1
     
-    
-    #plot(perm.p[, i],obs.p[, i],
-    #     pch="*", ylim = c(0,0.05), xlim = c(0,0.05),
-    #     xlab =(paste(adj,"pvalue by permutation")), 
-    #     ylab=(paste(adj,"pvalue by limma ")), col = "orange", 
-    #     main=(paste("significant genes for",colnames(obs.p)[i],"cells")))
-    #abline(a = 0,b=1, col="blue", lwd = 1.5)
-    
+    plot(ggplot(data=df[c(limma_upreg_genes,perm_upreg_genes), ],
+                aes(col=factor(true.DE)))+
+           geom_jitter(aes(x=df[c(limma_upreg_genes,perm_upreg_genes),"obs"],
+                           y=df[c(limma_upreg_genes,perm_upreg_genes), "perm"]),
+                       position=position_jitter(0.4))
+         +geom_hline(yintercept =0, col="black")+
+           ylab(paste(adj,"permutation pvalue"))+
+           #theme(legend.position= "none")
+      ylab(paste(adj,"permutation pvalue"))+
+      xlab(paste(adj,"limma pvalue")))
     #boxplot(perm.p[match( true.upreg[[i]], row.names(perm.p)), ],
     #        data = perm.p, 
     #        main=(paste("up-regulated genes for",colnames(perm.p)[i],"cells by permutation")),
@@ -157,7 +197,16 @@ computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.descri
     #        main=(paste("up-regulated genes for",colnames(perm.p)[i],"cells by limma")),
     #        names = colnames(obs.p))
     #abline(h=0.05, lty=2, col = "red", lwd = 2)
-
+    plot(ggplot(data=df[c(limma_upreg_genes,perm_upreg_genes), ],
+                aes(col=factor(true.DE)))+
+           geom_jitter(aes(x=df[c(limma_upreg_genes,perm_upreg_genes),"obs"],
+                           y=df[c(limma_upreg_genes,perm_upreg_genes), "perm"]),
+                       position=position_jitter(0.4))
+         +geom_hline(yintercept =0, col="black")+
+           ylab(paste(adj,"permutation pvalue"))+
+           #theme(legend.position= "none")+
+           xlim(0,0.05)+ylim(0,0.05)+
+           xlab(paste(adj,"limma pvalue")))
 
     if (length(perm_upreg_genes)>0){
       # plots for observation
@@ -193,9 +242,7 @@ computeSummary3<- function(obs.p, perm.p, adj, true.upreg, cell_num, main.descri
                  vertical=TRUE)
       abline(h=0.05, lty=2, col = "red", lwd = 2)
     }
-    
-    
-    #dev.off()
+
   }
   dev.off()
   return (summ)
